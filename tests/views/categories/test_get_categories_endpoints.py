1fdca1162611c8edc3a5d2b6abcfb19b9aadb1be
P.C Ndayisenga <pacifiqueclement@gmail.com>
2020-08-04 11:24:44 +0200

    Get single category

--- tests/views/categories/test_get_categories_endpoints.py
+++ tests/views/categories/test_get_categories_endpoints.py
@@ -1,7 +1,9 @@
 """ Module for testing get categories endpoints """
 
 import api.views.category
-from api.utils.helpers.messages.success import CATEGORIES_FETCHED_MSG
+from api.utils.helpers.messages.success import (CATEGORIES_FETCHED_MSG,
+                                                CATEGORY_FETCHED_MSG)
+from api.utils.helpers.messages.error import CATEGORY_NOT_FOUND_MSG
 from ...constants import API_BASE_URL
 
 
@@ -18,3 +20,23 @@ class TestGetCategoriesEndpoints:
         assert response.json['message'] == CATEGORIES_FETCHED_MSG
         assert 'categories' in response.json['data']
         assert len(response.json['data']['categories']) == 0
+
+    def test_get_single_category_succeeds(self, client, init_db, new_category):
+        """ Testing get single category """
+
+        new_category.save()
+        response = client.get(f'{API_BASE_URL}/categories/{new_category.name}')
+
+        assert response.status_code == 200
+        assert response.json['status'] == 'success'
+        assert response.json['message'] == CATEGORY_FETCHED_MSG
+        assert 'category' in response.json['data']
+
+    def test_get_single_category_with_unexisted_name_fails(self, client, init_db):
+        """ Testing get single category with unexisted name """
+
+        response = client.get(f'{API_BASE_URL}/categories/good')
+
+        assert response.status_code == 404
+        assert response.json['status'] == 'error'
+        assert response.json['errors'][0]['message'] == CATEGORY_NOT_FOUND_MSG
